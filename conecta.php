9f8b3523f3a8c74227c09411ed7dad1bd4c0407f
ProjectoInterjet <80373099+ProjectoInterjet@users.noreply.github.com>
2021-08-08 22:38:26 -0500

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,10 +1,14 @@
 <?php
 
 require 'vendor/autoload.php';
+/*
 $uri="mongodb+srv://user:user@costbd.stamk.mongodb.net/test";
 $bd="costbd";
 $client=new MongoDB\Client($uri);
-
+*/
+$client = new MongoDB\Client(
+    'mongodb+srv://user:<password>@costbd.stamk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+$db = $client->test;
 //$client = new MongoDB\Client(
   //  'mongodb+srv://cost:cost1745@cluster0.qzniz.mongodb.net/costbd?retryWrites=true&w=majority');
 
